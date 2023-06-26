82200e00d23ffd148cd4ade8ae00ac56a1696a74
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-06-26 19:33:48 +0100

    Feign env custom, docker image tag 12, deploy again on k8s, tested everything is ok except round roubin seems does not work

--- conversion-currency-k8s/src/main/java/com/jamesclear/first/microservice/CurrencyConversionMicroservice/Controller/CurrencyConversionProxy.java
+++ conversion-currency-k8s/src/main/java/com/jamesclear/first/microservice/CurrencyConversionMicroservice/Controller/CurrencyConversionProxy.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@FeignClient(name="currency-exchange")   if I want it talks to Eureka
 
 //CHANGE-KUBERNETES
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_CUSTOM}:8000")
 public interface CurrencyConversionProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
